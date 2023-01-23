67449aabd78722efea3752eea05337c2d325e3af
dukesvrtech <horace.reid@bluenetreflections.com>
2023-01-22 19:07:44 -0500

    Added endpoint to watch videos from yt player

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import * as Restify from 'restify';
 import dotenv from 'dotenv';
 import {resolve as resolvePath} from 'path';
 import App from './app';
+import {getVideoStreamFromYT} from "./services/yt-service";
 
 // add some generic error handlers here, to log any exceptions we're not expecting
 process.on('uncaughtException', err => console.log('uncaughtException', err));
@@ -16,10 +17,16 @@ process.on('unhandledRejection', reason => console.log('unhandledRejection', rea
 // Read .env if file exists
 dotenv.config();
 
-const sayHello = (req: Restify.Request, res: Restify.Response, next: Restify.Next) => {
-	console.log("Received Request", req.params);
-	res.send(`Hello ${req.params.path}`);
-	next();
+const streamVideo = async (req: Restify.Request, res: Restify.Response, next: Restify.Next) => {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const queryMap = require('querystring')?.parse(req.getQuery());
+	console.log("Received Watch Request", queryMap);
+	const v = queryMap?.v
+	if (!v) {
+		return res.send(400, 'Missing v request parameter')
+	}
+	const videoStream = await getVideoStreamFromYT(v);
+	res.redirect(302, videoStream.uri, next);
 }
 
 // This function starts the MRE server. It will be called immediately unless
@@ -37,7 +44,7 @@ function runApp() {
 		console.log("MRE is ready for Avatars")
 
 		// Setup up routes for server side processing.  Must configure host in environment variable.
-		server.adapter.server.get('/hello/:path', sayHello);
+		server.adapter.server.get('/watch', streamVideo);
 
 		// Handle new application sessions
 		server.adapter.onConnection((context, params) => {
